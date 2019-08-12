44a049bfc3a374ce417d7ee4b723bf078e488e38
Hemang <hemangshrimali22@gmail.com>
2019-08-12 14:57:10 +0000

    process

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 var express=require("express");
 var app=express();
 var bodyParser=require("body-parser");
+var bcrypt = require('bcryptjs');
 var mongoose=require("mongoose");
+var passport=require("passport");
+var LocalStrategy = require('passport-local').Strategy;
 var methodOverride=require("method-override");
 app.use(express.static("css"));  // static files in css ....
 app.use(methodOverride("_method"));
 
-// mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true}); ** this mongodb connects to local mongodb server **
+//mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});// ** this mongodb connects to local mongodb server **
 mongoose.connect('mongodb+srv://Hemang:Hemang%40123@blogs-uq9an.mongodb.net/test?retryWrites=true&w=majority',{useNewUrlParser: true});
 // this connect method uses mongodb atlas which is a hosting database...
 app.use(bodyParser.urlencoded({extended:true}));
 mongoose.set('useFindAndModify', false);
 
+// 1 model for blogs
 var blogSchema=new mongoose.Schema({
 	title:String,
 	image:String,
@@ -20,6 +24,63 @@ var blogSchema=new mongoose.Schema({
 });
 
 var Blog=mongoose.model("Blog",blogSchema);
+
+// 2 model for login system
+var UserSchema=new mongoose.Schema({
+	username:String,
+	password:String
+});
+
+var User=mongoose.model("User",UserSchema);
+
+// passport
+ 
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+	  // password match...
+	  bcrypt.compare(password,user.password,function(err,isMatch){
+		  if (err) throw err;
+		  if (isMatch) {
+            return done(null, user);
+          }  else {
+            return done(null, false, { message: 'Password incorrect' });
+          }
+	  });
+    });
+  }
+));
+
+ passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+ passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+// Express session
+app.use(require("express-session")({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// body parser...
+app.use(express.urlencoded({ extended: true }));
+
+//Routes.......................
 
 app.get("/",function(req,res){
    res.redirect("/blogs");	
@@ -35,11 +96,11 @@ app.get("/blogs",function(req,res){
 	});
 });
 
- app.get("/blogs/new",function(req,res){   // contains the form
+ app.get("/blogs/new", ensureAuthenticated,function(req,res){   // contains the form
       res.render("new.ejs");	 
  });
 
-app.get("/blogs/:id",function(req,res){
+app.get("/blogs/:id", ensureAuthenticated,function(req,res){
 	Blog.findById(req.params.id,function(err,found_blog){
 		if(err) console.log("error");
 		else{
@@ -62,7 +123,7 @@ app.post("/blogs",function(req,res){
 });
 
 // this will take you to a form previously filled to edit
-app.get("/blogs/:id/edit",function(req,res){
+app.get("/blogs/:id/edit", ensureAuthenticated,function(req,res){
 	Blog.findById(req.params.id,function(err,edit){
 		if(err) console.log("error");
 		else{
@@ -71,7 +132,7 @@ app.get("/blogs/:id/edit",function(req,res){
 	});
 });
 // main update happen here
-app.put("/blogs/:id",function(req,res){
+app.put("/blogs/:id", ensureAuthenticated,function(req,res){
 	var title=req.body.name;
 	var image=req.body.image;
 	var body=req.body.description;
@@ -85,7 +146,7 @@ app.put("/blogs/:id",function(req,res){
 	});
 });
 
-app.delete("/blogs/:id",function(req,res){
+app.delete("/blogs/:id", ensureAuthenticated,function(req,res){
    	    Blog.findByIdAndRemove(req.params.id,function(err,deleted){
 		if(err) console.log("error");
 		else{
@@ -94,12 +155,65 @@ app.delete("/blogs/:id",function(req,res){
 	});
 });
 
-// Blog.create({
-//	title:"Jee",
-//	image:"https://www.impactbnd.com/hubfs/blog-image-uploads/9_Blog_Layout_Best_Practices_From_2017.jpg",
-//	body:"Here comes our first BLog!!!"
-//});
+// Auth routes===========
+app.get("/register",forwardAuthenticated,function(req,res){
+	res.render("register.ejs");
+});
 
-app.listen(process.env.PORT,process.env.IP,function(){  // can run on any port
+app.post("/register",function(req,res){
+    var username=req.body.username;
+	var password=req.body.password;
+	User.findOne({username:username},function(err,user_found){
+		if(err) throw err;
+		else if(user_found){
+			res.redirect("/register");
+		}
+		else{ bcrypt.genSalt(10, function(err, salt){
+               bcrypt.hash(password, salt, function(err, hash){
+               if (err) throw err;
+                   var pass=hash;
+				   User.create({username:username,password:pass},function(err,user){
+		           console.log("successfully registered");
+			       res.redirect("/login");
+	              });
+			   });
+			})};	
+	});
+});
+
+app.get("/login",forwardAuthenticated,function(req,res){
+	res.render("login.ejs");
+});
+
+app.post('/login', (req, res, next) => {
+    passport.authenticate('local', {
+    successRedirect: '/blogs',
+    failureRedirect: '/login'
+  })(req, res, next);
+});
+
+app.get("/logout",function(req,res){
+	req.logout();
+	res.redirect("/");
+});
+ 
+// config auth
+  function forwardAuthenticated(req, res, next) {
+    if (!req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect('/blogs');      
+  };
+
+ function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect('/login');
+  };
+
+// Server Side.........
+var PORT=process.env.PORT || 3000;
+app.listen(PORT,process.env.IP,function(){  // can run on any port
 	console.log("Our Blog Server has started");
 });
